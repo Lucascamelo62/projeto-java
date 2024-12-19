f5ff73267f819e854dce857fe5e77048a8f265c1
Lucascamelo62 <lucascamelo62@gmail.com>
2024-12-19 11:47:35 -0300

    Utilizando a classe String

--- src/src/Main.java
+++ src/src/Main.java
@@ -8,10 +8,17 @@ public class Main {
 
         String filme = "Top GUN - Mavericks";
         int anoLancamento = 2022;
-        boolean inclusoLista = false;
+        boolean inclusoLista = true;
+        String sinopse = """
+                Filme mediano
+                Longa Duração
+                Aclamado pelos cinemas americanos
+                """;
+        //Calculo da média
+        double media = (7 + 8 + 10)/3;
 
         if (inclusoLista) {
-            System.out.println("Nome do filme: " + filme + "\n" + "Ano de Lançamento" + anoLancamento);
+            System.out.println("Nome do filme: " + filme + "\n" + "Ano de Lançamento" + anoLancamento + "\n" + sinopse + "\n" + "Média:" + media);
         } else {
             System.out.println("Nenhum filme disponível na lista");
         }

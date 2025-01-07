605b90ec8d1817190efc6641015634ace4b39ce9
Lucascamelo62 <lucascamelo62@gmail.com>
2025-01-07 14:50:32 -0300

    Incluindo loops

--- src/src/Leitura.java
+++ src/src/Leitura.java
@@ -4,13 +4,21 @@ public class Leitura {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        double nota = 0;
+        double mediaNotas = 0;
+
         System.out.println("Digite o nome do seu filme preferido:");
         String nomeFilme = sc.nextLine();
         System.out.println("Digite o ano de lançamento do filme:");
         int anoLancamento = sc.nextInt();
-        System.out.println("Informe a avaliação do filme");
-        double avaliacao = sc.nextDouble();
+        System.out.println("Digite as 3 melhores notas para o filme");
+        for (int i = 1; i < 4; i++) {
+            System.out.printf(String.format("Nota %d: ", i));
+            nota = sc.nextDouble();
+            mediaNotas += nota;
+        }
+        System.out.println(String.format("Informações gerais do filme: \nNome: %s \nAno de Lançamento: %d\nAvaliação: %.2f", nomeFilme, anoLancamento, (mediaNotas / 3)));
 
-        System.out.println(String.format("Informações gerais do filme: \nNome: %s \nAno de Lançamento: %d\nAvaliação: %.2f", nomeFilme, anoLancamento, avaliacao));
+        sc.close();
     }
 }

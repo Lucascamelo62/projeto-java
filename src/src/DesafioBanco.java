8e4a2ce605d2da70ad4e177fbd3da2ef9c2dd196
Lucascamelo62 <lucascamelo62@gmail.com>
2025-01-07 15:41:37 -0300

    Subindo tela inicial

---
+++ src/src/DesafioBanco.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DesafioBanco {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("***************************************");
+        System.out.println("Dados iniciais do cliente:\n");
+        System.out.println("Nome: Lucas Silva Camelo");
+        System.out.println("Tipo de Conta: Conta Corrente");
+        System.out.println("Saldo Inicial: 2500,00");
+        System.out.println("***************************************\n\n");
+
+        System.out.println("Operações\n");
+        System.out.println("1 - Consulta Saldo");
+        System.out.println("2 - Receber Valor");
+        System.out.println("3 - Transferir Valor");
+        System.out.println("4 - Sair");
+    }
+}

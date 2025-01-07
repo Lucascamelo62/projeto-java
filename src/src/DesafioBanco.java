32c0c0e9be9287f08f8974d39e19c55f41a48df9
Lucascamelo62 <lucascamelo62@gmail.com>
2025-01-07 15:51:35 -0300

    Incluindo tratativas na tela

--- src/src/DesafioBanco.java
+++ src/src/DesafioBanco.java
@@ -3,18 +3,30 @@ import java.util.Scanner;
 public class DesafioBanco {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int op = 0;
 
-        System.out.println("***************************************");
-        System.out.println("Dados iniciais do cliente:\n");
-        System.out.println("Nome: Lucas Silva Camelo");
-        System.out.println("Tipo de Conta: Conta Corrente");
-        System.out.println("Saldo Inicial: 2500,00");
-        System.out.println("***************************************\n\n");
+        while (op != 4) {
+            System.out.println("***************************************");
+            System.out.println("Dados iniciais do cliente:\n");
+            System.out.println("Nome: Lucas Silva Camelo");
+            System.out.println("Tipo de Conta: Conta Corrente");
+            System.out.println("Saldo Inicial: 2500,00");
+            System.out.println("***************************************\n\n");
 
-        System.out.println("Operações\n");
-        System.out.println("1 - Consulta Saldo");
-        System.out.println("2 - Receber Valor");
-        System.out.println("3 - Transferir Valor");
-        System.out.println("4 - Sair");
+            System.out.println("Operações\n");
+            System.out.println("1 - Consulta Saldo");
+            System.out.println("2 - Receber Valor");
+            System.out.println("3 - Transferir Valor");
+            System.out.println("4 - Sair\n");
+
+            System.out.printf("Digite a opção desejada: ");
+            op = sc.nextInt();
+            if (op < 1 || op > 4) {
+                System.out.println("Opção inválida!");
+            }
+
+        }
+
+        sc.close();
     }
 }

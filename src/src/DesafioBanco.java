515694953be5b04bf50a0cff91923dd9ef256bb3
Lucascamelo62 <lucascamelo62@gmail.com>
2025-01-08 15:42:57 -0300

    Melhorando menu

--- src/src/DesafioBanco.java
+++ src/src/DesafioBanco.java
@@ -3,24 +3,30 @@ import java.util.Scanner;
 public class DesafioBanco {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double saldoInicial = 2500;
-        double saldoFinal = saldoInicial;
+        String nomeCliente, tipoConta;
+        nomeCliente = "Lucas Camelo";
+        tipoConta = "Conta Corrente";
+        double saldoInicial, saldoFinal;
+        saldoInicial = 2500;
+        saldoFinal = saldoInicial;
         int op = 0;
+        String menu = """
+                Operações
+                1 - Consulta Saldo
+                2 - Receber Valor
+                3 - Transferir Valor
+                4 - Sair
+                """;
 
         while (op != 4) {
             System.out.println("***************************************");
             System.out.println("Dados iniciais do cliente:\n");
-            System.out.println("Nome: Lucas Silva Camelo");
-            System.out.println("Tipo de Conta: Conta Corrente");
+            System.out.println(String.format("Nome do Cliente: %s", nomeCliente));
+            System.out.println(String.format("Tipo de Conta: %s", tipoConta));
             System.out.println(String.format("Saldo Inicial: %.2f", saldoInicial));
             System.out.println("***************************************");
 
-            System.out.println("Operações\n");
-            System.out.println("1 - Consulta Saldo");
-            System.out.println("2 - Receber Valor");
-            System.out.println("3 - Transferir Valor");
-            System.out.println("4 - Sair\n");
-
+            System.out.println(menu);
             System.out.printf("Digite a opção desejada: ");
             op = sc.nextInt();
             if (op < 1 || op > 4) {
